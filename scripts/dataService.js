7b2c486e6f0ba0bbccd0bd06077353dcd3b30454
vegardmj <vegard.aas.mjelde@gmail.com>
2022-12-20 17:20:25 +0100

    test database

--- scripts/dataService.js
+++ scripts/dataService.js
@@ -1,15 +1,45 @@
-function getSubjects(){
-    return null;
-}
-function getVerbs(){
-    return null;
-}
 
 function testScript(){
     console.log("hei")
+    eadTextFile("../database/test.txt");
+}
+
+
+
+
+
+function convertToJSON(data){
+    return [];
+}
+
+
+function getSubjects(){
+    let data = readFile("../database/", "subjects.xls");
+    return convertToJSON(data);
+}
+function getVerbs(){
+    let data = readFile("../database/", "verbs.xls");
+    return convertToJSON(data);
 }
 
 function readFile(path, filename){
-
     return null;
+}
+
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
 }
